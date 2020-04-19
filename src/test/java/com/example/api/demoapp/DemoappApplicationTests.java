f2ee4549cf00ecf0394603c035e3bc091a8c98dc
viswanath <viswa.lsk@gmail.com>
2020-04-19 16:15:23 +0530

    Initial commit

---
+++ src/test/java/com/example/api/demoapp/DemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
